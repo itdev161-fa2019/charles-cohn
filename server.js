3c3e0704d364e74d6680e4410ecb543f027cb25f
Nekroun <cohncl@gmatc.matc.edu>
2019-10-01 00:11:34 -0500

    Assignment 3 with validation

--- server.js
+++ server.js
@@ -20,5 +20,15 @@ app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
+
+app.post('api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
 // Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
