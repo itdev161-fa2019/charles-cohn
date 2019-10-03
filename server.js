160abec15afa2d3e914fdf0320c0822385e55680
Nekroun <cohncl@gmatc.matc.edu>
2019-10-03 04:36:32 -0500

    Original code retyped due to difference in pull

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 // import { get } from 'https';
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
+import cors from 'cors';
 
 // Initialize express application
 const app = express();
@@ -9,9 +11,41 @@ const app = express();
 connectDatabase();
 
 // API endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) =>
 res.send('http get request sent to root api endpoint')
 );
 
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+
+ app.post(
+     '/api/users',
+     [
+         check('name', 'Please enter your name')
+            .not()
+            .isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or more characters'
+        ).isLength({ min: 6 })
+     ],
+     (req, res) => {
+         const errors = validationResult(req);
+         if (!errors.isEmpty()) {
+             return res.status(422).json({ errors: errors.array() });
+         } else {
+             return res.send(req.body);         
+         }
+     }
+ );
+
 // Connection listener
-app.listen(3000, () => console.log('Express server running on port 3000'));
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
